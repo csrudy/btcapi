4f05c63a27c572c546986d01b2c84bd69dad8326
Parker Allen <parkerjallen1@gmail.com>
2019-02-14 08:55:40 -0800

    MVP finished

--- popup.js
+++ popup.js
@@ -16,18 +16,24 @@ fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
       return Math.round((current - first) * 100) / 100;
     }
 
-    const vchange = vChangeFunc(currentDay, firstDay);
-    const pchange = pChangeFunc(firstDay, currentDay);
+    let vchange = vChangeFunc(currentDay, firstDay);
+    let pchange = pChangeFunc(firstDay, currentDay);
 
     const valueChange = document.createElement('div');
+    if (Math.sign(vchange)=== 1) {
+      valueChange.style.color = 'green';
+      vchange = `+$${vchange}`
+    } else {
+      valueChange.style.color = 'red';
+      vchange = Math.abs(vchange);
+      vchange = `-$${vchange}`
+    }
     valueChange.innerHTML = `${vchange}`;
     valueChange.id = "valueChanged";
     $('#value').append(valueChange);
 
-    if (Math.sign(vchange)=== 1) {
-      valueChange.style.color = 'green';
-    } else {
-      valueChange.style.color = 'red';
+    if (Math.sign(pchange)===1) {
+      pchange = `+${pchange}`
     }
 
     const percentChange = document.createElement('div')
@@ -37,8 +43,10 @@ fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
 
     if (Math.sign(pchange)=== 1) {
       percentChange.style.color = 'green';
+      $('.info').append('<img src ="btcup.gif"></img>')
     } else {
       percentChange.style.color = 'red';
+      $('.info').append('<img src ="btcdown.gif"></img>')
     }
     
   })
@@ -46,12 +54,27 @@ fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
   fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
   .then(response => response.json()) //turned the url data into js object
   .then((jsonData) => {
-    const price = Math.round(jsonData.bpi["USD"].rate_float * 100) / 100;
-    console.log(price);
+    let usdprice = Math.round(jsonData.bpi["USD"].rate_float * 100) / 100;
+    let eurprice = Math.round(jsonData.bpi["EUR"].rate_float * 100) / 100;
+    let gbpprice = Math.round(jsonData.bpi["GBP"].rate_float * 100) / 100;
     const currentPrice = document.createElement('div');
-    currentPrice.innerHTML = `${price}`;
-    currentPrice.id = "currentPrice";
+    const usdPrice = document.createElement('div');
+    // const eurPrice = document.createElement('div');
+    // const gbpPrice = document.createElement('div');
+    usdprice = usdprice.toString();
+    usdprice = usdprice[0] + ',' + usdprice.slice(1);
+    currentPrice.innerHTML = `$${usdprice}`;
+    // eurprice = eurprice.toString();
+    // eurprice = eurprice[0] + ',' + eurprice.slice(1);
+    // eurPrice.innerHTML = `$${eurprice}`;eurprice = eurprice.toString();
+    // gbpprice = gbpprice.toString();
+    // gbpprice = gbpprice[0] + ',' + gbpprice.slice(1);
+    // gbpPrice.innerHTML = `$${gbpprice}`;
     $('#current').append(currentPrice);
+    // $('#usd').append(usdPrice);
+    // $('#eur').append(eurPrice);
+    // $('#gbp').append(gbpPrice);
+    
   })
 
 
